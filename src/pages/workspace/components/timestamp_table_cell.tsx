7ec96da0db04c53dffb80a495299ee53c625aa44
Aleh Zasypkin <aleh.zasypkin@gmail.com>
2024-08-19 00:41:56 +0200

    feat(webhooks): improve responders list layout, display last requested timestamp

--- src/pages/workspace/components/timestamp_table_cell.tsx
+++ src/pages/workspace/components/timestamp_table_cell.tsx
@@ -5,21 +5,38 @@ import { EuiText, EuiToolTip } from '@elastic/eui';
  * The maximum difference in days between the current date and the timestamp for the timestamp to be displayed as a
  * relative timestamp.
  */
-const MAX_DIFF_FOR_RELATIVE_TIMESTAMP = -3;
+const MAX_DIFF_FOR_RELATIVE_TIMESTAMP_DAYS = -3;
+
+/**
+ * The threshold in minutes for a timestamp to be considered recent (used for highlighting).
+ */
+const RECENT_HIGHLIGHT_THRESHOLD_MINUTES = -60;
 
 export interface Props {
   timestamp: number;
   color?: string;
+  highlightRecent?: boolean;
 }
 
-export function TimestampTableCell({ timestamp, color }: Props) {
+export function TimestampTableCell({ timestamp, color, highlightRecent }: Props) {
   const unixTimestamp = unix(timestamp);
+  const isRecent = highlightRecent && unixTimestamp.diff(now(), 'minutes') >= RECENT_HIGHLIGHT_THRESHOLD_MINUTES;
+
+  // Detect if the text should be highlighted, unless fixed color is provided.
+  let textColor = color;
+  if (isRecent && !textColor) {
+    textColor = 'danger';
+  }
+
+  const formattedTimestamp =
+    isRecent || unixTimestamp.diff(now(), 'days') > MAX_DIFF_FOR_RELATIVE_TIMESTAMP_DAYS
+      ? unixTimestamp.fromNow(false)
+      : unixTimestamp.format('LL');
+
   return (
-    <EuiToolTip content={unixTimestamp.format('ll HH:mm')}>
-      <EuiText size={'s'} color={color}>
-        {unixTimestamp.diff(now(), 'days') > MAX_DIFF_FOR_RELATIVE_TIMESTAMP
-          ? unixTimestamp.fromNow(false)
-          : unixTimestamp.format('LL')}
+    <EuiToolTip content={unixTimestamp.format('ll HH:mm:ss')}>
+      <EuiText size={'s'} color={textColor}>
+        {isRecent ? <b>{formattedTimestamp}</b> : formattedTimestamp}
       </EuiText>
     </EuiToolTip>
   );
